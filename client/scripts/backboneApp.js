b817575ae54ee7ab2e191f81fb29f5fd3cc7d675
alexmclean <alexmclean206@berkeley.edu>
2015-10-20 19:52:23 -0700

    factored pieces of freds solution to make our backbone work

--- client/scripts/backboneApp.js
+++ client/scripts/backboneApp.js
@@ -7,76 +7,118 @@ var App = Backbone.Model.extend({
 });
 */
 var Chat = Backbone.Model.extend({
-  initialize: function(userName, message, roomName) {
-    this.set('userName', userName);
-    this.set('message', message);
-    this.set('roomName', roomName);
+  url: 'https://api.parse.com/1/classes/chatterbox',
+
+  defaults: {
+    username: '',
+    text: '',
+    roomName: 'All Rooms'
   },
 
-  defaults: {roomName : 'All Rooms'}
-
+  //isn't called :(
+  addFriend: function(){
+    console.log("added a friend!");
+  }
 });
 
-/*
-var ChatView = Backbone.View.extend({
+var Chats = Backbone.Collection.extend({
+  model: Chat,
+  url: 'https://api.parse.com/1/classes/chatterbox',
+
   initialize: function(){
-
+    //doesnt belong here
+    this.getMessages();
   },
 
+  getMessages: function(){
+    console.log("getting more chats...");
+    this.fetch({data: {order: '-createdAt'}});
+  },
+  parse: function(response, options){
+    return response.results;
+  }
+});
+
+//CHATVIEW
+var ChatView = Backbone.View.extend({
+
+  initialize: function() {
+    this.model.on('change', this.render, this);
+  },
+
+  template: _.template('<div class="chat" data-id="<%- objectId %>"> \
+                          <div class="username"><%- username %></div> \
+                          <div class="text"><%- text %></div> \
+                        </div>'),
   render: function(){
-    var html = [
-      '<div class="chat">',
-        this.model.get('message'),
-        '<div class="username">',
-          this.model.get('userName'),
-        '</div>',
-      '</div>'].join('');
-    return this.$el.html(html);
+    this.$el.html(this.template(this.model.attributes));
+    return this.$el;
   },
-
   addFriend: function(){
     console.log("added a friend!");
   },
-
   events: {'click .username': 'addFriend'}
 
 });
-*/
 
-var Chats = Backbone.Collection.extend({
-  model: Chat
-});
 
 var ChatsView = Backbone.View.extend({
+
   initialize: function(){
-    this.collection.on('click', function(e){
-      this.model.addFriend();
-    });
+    this.collection.on('sync', this.render, this);
+    this.onScreen = {};
   },
 
   render: function(){
-    var html = [
-      '<div id="chats">',
-      '</div>'
-    ].join('');
+    this.collection.forEach(this.renderMessage, this);
+  },
 
-    this.$el.html(html);
-
-    this.$el.find('#chats').append(this.collection.map(function(chat){
-      var chathtml = [
-      '<div class="chat">',
-        chat.get('message'),
-        '<div class="username">',
-          chat.get('userName'),
-        '</div>',
-      '</div>'].join('');
-      return chathtml;
-    }));
-    return this.$el;
-  }
+  renderMessage: function(chat){
+    if(!this.onScreen[chat.get('objectId')]){
+      var chatView = new ChatView({model: chat});
+      this.$el.prepend(chatView.render());
+      this.onScreen[chat.get('objectId')] = true;
+    }
+  },
 
 });
 
+var FormView = Backbone.View.extend({
+
+  initialize: function() {
+    this.collection.on('sync', this.stopSpinner, this);
+  },
+
+  events: {
+    'click #send': 'handleSubmit'
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    this.startSpinner();
+    console.log(this.collection);
+    var $text = this.$('#message');
+    this.collection.create({
+      username: window.location.search.substr(10),
+      text: $text.val()
+    });
+    console.log(this.collection);
+    $text.val('');
+  },
+
+  startSpinner: function() {
+    this.$('.spinner img').show();
+    this.$('form input[type=submit]').attr('disabled', "true");
+  },
+
+  stopSpinner: function() {
+    this.$('.spinner img').fadeOut('fast');
+    this.$('form input[type=submit]').attr('disabled', null);
+  }
+
+});
+/*
 var chat = new Chat('Alex', 'he says hi', 'MyRoom');
 var chat2 = new Chat('Alext', 'he says hih', 'MyRoom');
 var chat3 = new Chat('Alexa', 'he says hig', 'MyRoom');
@@ -86,3 +128,4 @@ var chatColl = new Chats(chatList);
 var chatsview = new ChatsView({collection : chatColl});
 
 $('body').append(chatsview.render());
+*/
